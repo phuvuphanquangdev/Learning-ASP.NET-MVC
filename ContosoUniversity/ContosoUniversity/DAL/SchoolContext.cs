4cec0e8476479259ecd094ded4a3fdffdcf0432c
Phu Vu Phan Quang <phu.vuphanquang@gmail.com>
2024-10-28 01:35:46 +0700

    Commit 18 - EF - Implement Inheritance

--- ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
@@ -21,6 +21,7 @@ namespace ContosoUniversity.DAL
         public DbSet<Department> Departments { get; set; }
         public DbSet<Instructor> Instructors { get; set; }
         public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<Person> People { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
--- ContosoUniversity/ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -8,32 +8,14 @@ using System.Web;
 
 namespace ContosoUniversity.Models
 {
-    public class Student
+    public class Student : Person
     {
-        public int ID { get; set; }
-
-        [Required]
-        [Display(Name = "Last Name")]
-        [StringLength(50, ErrorMessage = "Last Name cannot be longer than 50 characters")]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$", ErrorMessage = "Alphabet only and first letter must be capital")] //First Letter Capital rest ar al
-        public string LastName { get; set; }
-
-        [Required]
-        [Display(Name = "First Name")]
-        [StringLength(50, ErrorMessage = "First Name cannot be longer than 50 characters")]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$", ErrorMessage = "Alphabet only and first letter must be capital")]
-        [Column("FirstName")]
-        public string FirstMidName { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
 
-        [Display(Name = "Full Name")]
-        public string FullName {
-            get { return LastName + ", " + FirstMidName; }
-        }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
