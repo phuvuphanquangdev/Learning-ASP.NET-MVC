5b6c79eec5455780bcd45c162a2836a0c8006582
Phu Vu Phan Quang <phu.vuphanquang@gmail.com>
2024-10-28 01:27:42 +0700

    Commit 14 - EF Store Procedure & Asyncronyse Data

--- ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
@@ -31,6 +31,8 @@ namespace ContosoUniversity.DAL
                 .Map(t => t.MapLeftKey("CourseID")
                     .MapRightKey("InstructorID")
                     .ToTable("CourseInstructor"));
+
+            modelBuilder.Entity<Department>().MapToStoredProcedures();
         }
 
 
